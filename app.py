10da1724c9397fd91d6336210c36924efc862d49
Ubuntu <ubuntu@ip-172-31-88-132.ec2.internal>
2023-01-10 14:53:07 +0000

    Deploy python app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import socket
+app = Flask(__name__)
+
+@app.route("/")
+def get_hostname():
+  hostname = socket.gethostname()
+  return f'The hostname of the server is: {hostname}'
+
+if __name__  == '__main__':
+  app.run(host='0.0.0.0')
+
